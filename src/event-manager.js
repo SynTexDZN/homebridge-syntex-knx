939034890e17684d3e3354cf717b48f03a65a9a1
SynTex <syntexdzn@t-online.de>
2021-12-07 21:39:31 +0100

    Bug Fix

--- src/event-manager.js
+++ src/event-manager.js
@@ -15,7 +15,7 @@ module.exports = class EventManager extends EventEmitter
 	{
 		super.on(stream, (source, destination, value) => {
 			
-			if(source.id != sender.id && destination == receiver)
+			if(source != sender && destination == receiver)
 			{
 				callback(value);
 
